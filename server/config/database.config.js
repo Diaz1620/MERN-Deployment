52e4d4ccee035ea1767e9fc25de8e89f794feddc
Diaz1620 <77040700+Diaz1620@users.noreply.github.com>
2021-03-28 16:45:28 -0400

    Delete server directory

--- server/config/database.config.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose'),
-    db = process.env.DB_URI;
-
-mongoose.connect(`mongodb://localhost/${db}`,{
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-})
-    .then( () => console.log("You are now in the mainframe...") )
-    .catch(err => console.log(`Metldown Meltdown Meltdown`,err))
